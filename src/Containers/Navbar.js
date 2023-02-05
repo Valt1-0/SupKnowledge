c58c010c43f1316c3143dc060b6a3bfae3fec023
Valentin <lai2val1@gmail.com>
2023-02-05 18:46:26 +0100

    oui

--- src/Containers/Navbar.js
+++ src/Containers/Navbar.js
@@ -1,40 +1,140 @@
 import React from "react";
+import { Disclosure } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
-import SearchIcon from "../Assets/img/search.svg";
+import logo from "../Assets/img/SupKnowledge2.png"
+
+const navigation = [
+  { name: "Dashboard", href: "#", current: true },
+  { name: "Team", href: "#", current: false },
+  { name: "Projects", href: "#", current: false },
+  { name: "Calendar", href: "#", current: false },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 const Navbar = () => {
   return (
     <>
-      <div class="px-4 mt-3 sm:px-8 lg:px-16 xl:px-20 mx-auto">
-        <div class="bg-white rounded flex items-center w-3/6 p-3 shadow-sm border border-gray-200">
-          <button class="outline-none focus:outline-none">
-            <img src={SearchIcon} alt=""></img>
-          </button>
-          <input
-            type="search"
-            name=""
-            id=""
-            placeholder="Search ..."
-            x-model="q"
-            class="w-full pl-4 text-sm outline-none focus:outline-none bg-transparent"
-          />
-          <div class="select">
-            <select
-              name=""
-              id=""
-              x-model="image_type"
-              class="text-sm outline-none focus:outline-none bg-transparent"
-            >
-              <option value="all" selected>
-                All
-              </option>
-              <option value="photo">Photo</option>
-              <option value="illustration">Illustration</option>
-              <option value="vector">Vector</option>
-            </select>
-          </div>
-        </div>
-      </div>
+      <Disclosure as="nav" className="bg-gray-200">
+        {({ open }) => (
+          <>
+            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+              <div className="relative flex h-20 items-center justify-between">
+                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                  {/* Mobile menu button*/}
+                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                  </Disclosure.Button>
+                </div>
+
+                <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                  <div className="flex flex-shrink-0 items-center">
+                    <img
+                      className="block h-14 w-auto lg:hidden"
+                      src={logo}
+                      alt="Your Company"
+                    />
+                    <img
+                      className="hidden h-8 w-auto lg:block"
+                      src={logo}
+                      alt="Your Company"
+                    />
+                    <span className="ml-4">
+                      <strong>SupKnowledge</strong>
+                    </span>
+                  </div>
+
+                  <div className="hidden sm:ml-6 sm:block">
+                    <div className="flex space-x-4">
+                      {/* Start - Search Bar */}
+                      <div className="bg-white rounded flex items-center min-w-max p-1 shadow-sm border border-gray-200">
+                        <button className="outline-none focus:outline-none">
+                          <svg
+                            className=" w-5 text-gray-600 h-5 cursor-pointer"
+                            fill="none"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                          </svg>
+                        </button>
+                        <input
+                          type="search"
+                          placeholder="Search ..."
+                          
+                          className="w-full pl-4 text-sm outline-none text-gray-600 focus:outline-none bg-transparent"
+                        />
+                        <div className="select">
+                          {/* <select
+                            x-model="image_type"
+                            class="text-sm outline-none focus:outline-none bg-transparent"
+                          >
+                            <option value="all" selected>
+                              All
+                            </option>
+                            <option value="photo">Photo</option>
+                            <option value="illustration">Illustration</option>
+                            <option value="vector">Vector</option>
+                          </select> */}
+                        </div>
+                      </div>
+                      {/* End - Search Bar */}
+
+                      {navigation.map((item) => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className={classNames(
+                            item.current
+                              ? "bg-gray-700 text-white"
+                              : "text-gray-300 hover:bg-gray-500 hover:text-white",
+                            "px-3 py-2 rounded-md text-sm font-medium"
+                          )}
+                          aria-current={item.current ? "page" : undefined}
+                        >
+                          {item.name}
+                        </a>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <Disclosure.Panel className="sm:hidden">
+              <div className="space-y-1 px-2 pt-2 pb-3">
+                {navigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className={classNames(
+                      item.current
+                        ? "bg-gray-700 text-white"
+                        : "text-gray-300 hover:bg-gray-500 hover:text-white",
+                      "block px-3 py-2 rounded-md text-base font-medium"
+                    )}
+                    aria-current={item.current ? "page" : undefined}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                ))}
+              </div>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
     </>
   );
 };
