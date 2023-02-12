5c429b11f3bb96570404269340a9c42572fdc4af
Valentin <lai2val1@gmail.com>
2023-02-13 00:41:38 +0100

    404 effect

--- src/Containers/404.js
+++ src/Containers/404.js
@@ -1,20 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Illustration404 from "../Assets/img/404Illustration.png";
+import { motion } from "framer-motion";
 
 const Error404 = () => {
+  const [imageLoading, setImageLoading] = useState(true);
+  const [pulsing, setPulsing] = useState(true);
+
+  const imageLoaded = () => {
+    setImageLoading(false);
+    setTimeout(() => setPulsing(false), 600);
+  };
+
   return (
     <>
-      <div className="justify-center items-center">
-        <div className="flex font-bold ">
-          <div>4</div>
-          <div>0</div>
-          <div>4</div>
-        </div>
-        Error
-        <img src={Illustration404} alt="" />
+      <div className="grid place-items-center mt-10">
+        <motion.img
+          initial={{ height: "16rem", opacity: 0 }}
+          // style={{ height: imageLoading ? "6rem" : "auto" }}
+          animate={{
+            height: imageLoading ? "16rem" : "auto",
+            opacity: imageLoading ? 0 : 1,
+          }}
+          transition={
+            ({ height: { delay: 0, duration: 0.4 } },
+            { opacity: { delay: 0.4, duration: 0.4 } })
+          }
+          onLoad={imageLoaded}
+          src={Illustration404}
+        />
+        <motion.span
+          className="font-bold text-7xl"
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.8,
+            delay: 0.5,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
+        >
+          404
+        </motion.span>
+
+        <motion.span
+          className="font-bold text-8xl"
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.8,
+            delay: 0.7,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
+        >
+          Error
+        </motion.span>
+      </div>
+      <div className="flex justify-center items-center mt-5">
         {/* <Link to="/"> */}
-          <button className="bg-teal-300 rounded">Back to the Menu</button>
+        <motion.button
+          className="bg-slate-200 rounded hover:bg-slate-400 text-black hover:text-white font-bold py-2 px-4"
+          whileHover={{ scale: 1.1 }}
+          initial={{ opacity: 0}}
+          animate={{ opacity: 1}}
+          transition={{
+            type: "spring",
+            stiffness: 400,
+            damping: 10,
+            duration: 0.8,
+            delay: 0.9,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
+        >
+          Back to the Menu
+        </motion.button>
         {/* </Link> */}
       </div>
     </>
